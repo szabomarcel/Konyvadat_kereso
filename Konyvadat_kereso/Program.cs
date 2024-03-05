6b67ab5d4eb4e0ed8d224908f1d16fa26a201d72
szabomarcel <szabomarcell34@gmail.com>
2024-03-05 18:04:47 +0100

    Add project files.

---
+++ Konyvadat_kereso/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using konyv;
+
+namespace Konyvadat_kereso
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Adja meg az ISBN könyv számát:");
+            string isbn = Console.ReadLine();
+            await book(isbn);
+            Console.ReadLine();
+        }
+
+        private static async Task book(string isbn)
+        {
+            string apiUrl = $"https://www.goodreads.com/book/isbn/isbn?key=apiKey";
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    // Itt feldolgozhatod a jsonResponse-t, például kiírhatod a konzolra
+                    Console.WriteLine(jsonResponse);
+                }
+                else
+                {
+                    Console.WriteLine("Hiba történt a kérés során.");
+                }
+            }
+        }
+    }
+}
